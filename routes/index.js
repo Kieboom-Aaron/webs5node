d01fa4bbd19c1ed237c8ec3fc284514b5ac83921
Ivan Horn <ivanhorn1990@gmail.com>
2015-03-31 15:07:40 +0200

    nog steeds oauth test

--- routes/index.js
+++ routes/index.js
@@ -9,32 +9,38 @@ var Users = mongoose.model('Users');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: 'Express'
+    });
 });
-	
+
 var StrategyGoogle = require('passport-google-openidconnect').Strategy;
 passport.use(new StrategyGoogle({
-    clientID: '337957115280-b8ovrndgl8ge4arrvgn3idgbtdnuf2l0.apps.googleusercontent.com',
-    clientSecret: 'jeIcdXZOGQFWXbfD8e_fiL73',
-    callbackURL: 'http://battlerank.herokuapp.com/auth/google/callback',
-    skipUserProfile: true // doesn't fetch user profile
-  },
-  function(iss, sub, profile, accessToken, refreshToken, done) {
-    Users.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return done(err, user);
-    });
-  }
+        clientID: '337957115280-b8ovrndgl8ge4arrvgn3idgbtdnuf2l0.apps.googleusercontent.com',
+        clientSecret: 'jeIcdXZOGQFWXbfD8e_fiL73',
+        callbackURL: 'http://battlerank.herokuapp.com/auth/google/callback',
+        skipUserProfile: true // doesn't fetch user profile
+    },
+    function(iss, sub, profile, accessToken, refreshToken, done) {
+        Users.findAndModify({
+            googleId: profile.id
+        }, function(err, user) {
+            return done(err, user);
+        });
+    }
 ));
 
 router.get('/auth/google',
-  passport.authenticate('google-openidconnect'));
+    passport.authenticate('google-openidconnect'));
 
-router.get('/auth/google/callback', 
-  passport.authenticate('google-openidconnect', { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/');
-  });
+router.get('/auth/google/callback',
+    passport.authenticate('google-openidconnect', {
+        failureRedirect: '/login'
+    }),
+    function(req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/');
+    });
 
 
 
