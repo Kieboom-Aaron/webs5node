4b064bd1c529c80b6e9fbaf02b4ccbc986aa7528
kieboom-aaron <aaronkieboom@gmail.com>
2015-05-14 15:07:11 +0200

    added session secret

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('express-session')());
+app.use(require('express-session')({secret:'keyboardcat'}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
